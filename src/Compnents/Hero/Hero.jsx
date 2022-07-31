25d98579f5f31f600200cd5e7fab4ae17c411220
amir ali <amirgull42@gmail.com>
2022-07-31 09:23:38 -0700

    navBar Updated

--- src/Compnents/Hero/Hero.jsx
+++ src/Compnents/Hero/Hero.jsx
@@ -12,19 +12,16 @@ const Hero = () => {
         <div className="flex flex-col  gap-8   mt-5 ">
           <span
             className=" 
-               text-3xl       
-            md:text-6xl  font-bold text-purple"
+               text-[30px]      
+            md:text-[60px]  font-bold text-purple"
             id="introheading"
           >
             <span className="">Th</span>e Most Secure way to Launch, Invest, and
             Audit Blockchain Projects
           </span>
           <span id="ptext" className="text-[24px]  font-thin	 text-purple">
-            Your one stop shop. Our user-friendly interface provides a smooth
-            and easy process operable by even the most novice of crypto users.
-            All our functions can be implemented without writing a single line
-            of code along with implementing the highest security against
-            impostors.
+            Venify aims to be the premier IDO Launchpad for cryptocurrency
+            startups across all major blockchains.
           </span>
 
           <button className="  bg-blue text-white font-bold  p-4 w-[12rem] rounded-2xl   ">
@@ -35,14 +32,14 @@ const Hero = () => {
           <img
             src={intro}
             alt={intro}
-            className="w-[150rem] md:w-[180rem] mt-[-5rem] md:ml-[2rem] relative"
+            className="w-[100rem] md:w-[100rem] mt-[-5rem] md:ml-[2rem] relative"
           />
         </div>
       </div>
-      <div className=" blue1  bg-blue shadow-2xl  hidden md:flex"></div>
+      {/* <div className=" blue1  bg-blue shadow-2xl  hidden md:flex"></div>
       <div className=" blue2  bg-purple shadow-2xl  hidden md:flex"></div>
       <div className=" blue3  bg-orange shadow-2xl  hidden md:flex"></div>
-      <div className=" blue4  bg-blue shadow-2xl  hidden md:flex"></div>
+      <div className=" blue4  bg-blue shadow-2xl  hidden md:flex"></div> */}
     </div>
   );
 };
--- src/Compnents/Navbar/Navbar.jsx
+++ src/Compnents/Navbar/Navbar.jsx
@@ -1,50 +1,159 @@
 /** @format */
 
-import React from "react";
+import React, { useState } from "react";
 import logo from "../../assets/logo.png";
 import bsc from "../../assets/bsc.png";
 import { Link } from "react-router-dom";
+import { Transition } from "@headlessui/react";
+
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
   return (
-    <nav className="relative container mx-auto p-6">
-      <div className="flex items-center justify-between">
-        {/* logo */}
-        <div className="p-2 flex items-center">
-          <img src={logo} alt={logo} className="w-30" />
-          <Link to="/">
-            <span className=" text-2xl font-bold text-blue"> Venify</span>
-          </Link>
-        </div>
-        {/* items */}
-        <div className="hidden  md:flex space-x-6">
-          <Link to="/CreateToken" className=" text-navblue font-semibold ">
-            Create Token
-          </Link>
-          <Link to="/Launchpad" className=" text-navblue font-semibold ">
-            Create Launchpad
-          </Link>
-          <Link to="/CreateToken" className=" text-navblue font-semibold ">
-            Create Fairlaunch
-          </Link>
-          <Link to="/presale/" className=" text-navblue  font-semibold">
-            Presale List
-          </Link>
-          <Link to="/Saving" className=" text-navblue font-semibold ">
-            Venify Lock
-          </Link>
-        </div>
-        {/* button */}
-        <div
-          className=" flex items-center justify-center
+    <div className="main">
+      <nav className="relative container mx-auto p-6">
+        <div className="flex items-center justify-between">
+          {/* logo */}
+          <div className="p-2 flex items-center">
+            <img src={logo} alt={logo} className="w-30" />
+            <Link to="/">
+              <span className=" text-2xl font-bold text-blue"> Venify</span>
+            </Link>
+          </div>
+          {/* items */}
+          <div className="hidden md:flex space-x-6">
+            <Link to="/CreateToken" className=" text-navblue font-semibold ">
+              Create Token
+            </Link>
+            <Link to="/Launchpad" className=" text-navblue font-semibold ">
+              Create Launchpad
+            </Link>
+            <Link to="/CreateToken" className=" text-navblue font-semibold ">
+              Create Fairlaunch
+            </Link>
+            <Link to="/presale/" className=" text-navblue  font-semibold">
+              Presale List
+            </Link>
+            <Link to="/Saving" className=" text-navblue font-semibold ">
+              Venify Lock
+            </Link>
+          </div>
+          {/* button */}
+          <div
+            className=" hidden md:flex items-center justify-center
          bg-lightBlue p-2 px-6 rounded-full shadow-lg  "
-        >
-          <img src={bsc} alt={bsc} className="scale-75" />
-          <Link to="#">
-            <span className="font-medium text-blue">Connect Wallet</span>
-          </Link>
+          >
+            <img src={bsc} alt={bsc} className="scale-75" />
+            <Link to="#">
+              <span className="font-medium text-blue">Connect Wallet</span>
+            </Link>
+          </div>
+
+          <div className="-mr-2 flex md:hidden">
+            <button
+              onClick={() => setIsOpen(!isOpen)}
+              type="button"
+              className="bg-gray-900 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-blue hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
+              aria-controls="mobile-menu"
+              aria-expanded="false"
+            >
+              <span className="sr-only">Open main menu</span>
+              {!isOpen ? (
+                <svg
+                  className="block h-6 w-6"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4 6h16M4 12h16M4 18h16"
+                  />
+                </svg>
+              ) : (
+                <svg
+                  className="block h-6 w-6"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                </svg>
+              )}
+            </button>
+          </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+
+      {/*  mobile menu */}
+      <Transition
+        show={isOpen}
+        enter="transition ease-out duration-100 transform"
+        enterFrom="opacity-0 scale-95"
+        enterTo="opacity-100 scale-100"
+        leave="transition ease-in duration-75 transform"
+        leaveFrom="opacity-100 scale-100"
+        leaveTo="opacity-0 scale-95"
+      >
+        <div className="md:hidden  px-3 pb-4" id="mobile-menu">
+          <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
+            <Link
+              to="/CreateToken"
+              className="hover:text-black   text-blue block px-3 py-2  font-medium"
+            >
+              Create Token
+            </Link>
+
+            <Link
+              to="/Launchpad"
+              className="hover:text-black   text-blue block px-3 py-2  font-medium"
+            >
+              Create Launchpad
+            </Link>
+
+            <Link
+              to="Fairlaunch"
+              className="hover:text-black   text-blue block px-3 py-2  font-medium"
+            >
+              Create Fairlaunch
+            </Link>
+
+            <Link
+              to="/presale/"
+              className="hover:text-black   text-blue block px-3 py-2  font-medium"
+            >
+              Presale List
+            </Link>
+
+            <Link
+              to="/Saving"
+              className="hover:text-black   text-blue block px-3 py-2  font-medium"
+            >
+              Venify Lock
+            </Link>
+          </div>
+          <div
+            className="  flex items-center justify-center
+         bg-lightBlue p-2 px-6 rounded-full shadow-lg  "
+          >
+            <img src={bsc} alt={bsc} className="scale-75" />
+            <Link to="#">
+              <span className="font-medium text-blue">Connect Wallet</span>
+            </Link>
+          </div>
+        </div>
+      </Transition>
+    </div>
   );
 };
 
